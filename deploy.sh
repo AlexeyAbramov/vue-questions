98bb27f29ab70a92ec8c826ecb195577b5803394
AlexeyAbramov <abramov.alexey.93@ya.ru>
2020-03-17 01:10:58 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -15,11 +15,6 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-
-# если вы публикуете по адресу https://<USERNAME>.github.io
- #git push -f git@github.com:AlexeyAbramov/AlexeyAbramov.github.io.git master
-
-# если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:AlexeyAbramov/vue-questions.git master:gh-pages
 
 cd -
